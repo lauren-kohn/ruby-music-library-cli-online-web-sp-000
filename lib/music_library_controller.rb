b3684a0fe78cad31ac8c459115956d783757c2a2
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-13 16:24:14 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -20,14 +20,14 @@ class MusicLibraryController
       puts "What would you like to do?"
       if input == "list songs"
         list_songs
-      #elsif input == "list artists"
-      #  list_artists
-      #elsif input == "list genres"
-      #  list_genres 
-      #elsif input == "list artist"
-      #  list_songs_by_artist 
-      #elsif input == "list genre"
-      #  list_songs_by_genre 
+      elsif input == "list artists"
+        list_artists
+      elsif input == "list genres"
+        list_genres 
+      elsif input == "list artist"
+        list_songs_by_artist 
+      elsif input == "list genre"
+        list_songs_by_genre 
       elsif input == "play song"
         play_song
       end
