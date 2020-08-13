2f76500440d8a25ee469fd81c31109bd7baac105
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-13 16:20:27 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -18,6 +18,7 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
+      input = gets.strip
       if input == "list songs"
         list_songs
       elsif input == "list artists"
